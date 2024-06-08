8143fb969ceca811c8c1f6fb56d600365a61746b
espanderlof <jaime717@hotmail.es>
2024-06-08 17:55:25 -0400

    Crear componentes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { CategoriaComponent } from './components/categoria/categoria.component';
+import { RegistrarmeComponent } from './components/registrarme/registrarme.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    // {path: '', redirectTo: 'index', pathMatch: 'full'},
+    { path: '', component: HomeComponent },
+    { path: 'index', component: HomeComponent },
+    { path: 'categoria/:id', component: CategoriaComponent },
+    { path: 'registrarme', component: RegistrarmeComponent },
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+export class AppRoutingModule { }
