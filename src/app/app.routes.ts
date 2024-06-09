0f7de751602c3fdcbce1106aed7b526244c0c39d
espanderlof <jaime717@hotmail.es>
2024-06-09 17:34:15 -0400

    navbar y footer completo

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,13 +4,20 @@ import { HomeComponent } from './components/home/home.component';
 import { CategoriaComponent } from './components/categoria/categoria.component';
 import { RegistrarmeComponent } from './components/registrarme/registrarme.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { CartComponent } from './components/cart/cart.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { CategoriasComponent } from './components/categorias/categorias.component';
 
 export const routes: Routes = [
     // {path: '', redirectTo: 'index', pathMatch: 'full'},
     { path: '', component: HomeComponent },
-    { path: 'index', component: HomeComponent },
-    { path: 'categoria/:id', component: CategoriaComponent },
+    // { path: 'home', component: HomeComponent },
+    // { path: 'navbar', component: NavbarComponent },
+    // { path: 'categoria/:id', component: CategoriaComponent },
+    { path: 'categorias', component: CategoriasComponent },
     { path: 'registrarme', component: RegistrarmeComponent },
+    { path: 'cart', component: CartComponent },
+    // { path: 'footer', component: FooterComponent },
 ];
 
 @NgModule({
