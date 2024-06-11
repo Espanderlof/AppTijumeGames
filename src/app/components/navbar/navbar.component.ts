19ef3ac4bcd1539a2042aafceff6770b9ed06a21
espanderlof <jaime717@hotmail.es>
2024-06-10 20:57:16 -0400

    navbar fix categorías
    Fix del navbar el cual no pintaba de blanco al estar seleccionada una categoria.

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,5 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { Router, NavigationEnd, Event } from '@angular/router';
+import { filter } from 'rxjs/operators';
+
+function isNavigationEnd(event: Event): event is NavigationEnd {
+  return event instanceof NavigationEnd;
+}
 
 @Component({
   selector: 'app-navbar',
@@ -8,6 +14,22 @@ import { RouterModule } from '@angular/router';
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
+  isCategoryActive: boolean = false;
 
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    this.router.events.pipe(
+      filter(isNavigationEnd)
+    ).subscribe((event: NavigationEnd) => {
+      this.isCategoryActive = this.checkIfCategoryActive(event.urlAfterRedirects);
+    });
+  }
+
+  private checkIfCategoryActive(url: string): boolean {
+    return url.startsWith('/categoria/');
+  }
 }
+
+
